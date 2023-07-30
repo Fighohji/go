0674054d0cb29d5f320b42212c25bc75f0e68d9a
Fighohji <745046984@qq.com>
2023-07-30 18:07:21 +0800

    go

---
+++ test/GuessNumber.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	maxNum := 100
+	rand.Seed(time.Now().UnixNano())
+	secretNumber := rand.Intn(maxNum)
+	fmt.Println("The secret number is ", secretNumber)
+	fmt.Println("Please input your guess")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("An error occured while reading input. Please try again", err)
+			continue
+		}
+		input = strings.TrimSpace(input)
+		// fmt.Println(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Invalid input. Please enter an integer value")
+			continue
+		}
+		fmt.Println("Your guess is ", guess)
+		if guess > secretNumber {
+			fmt.Println("Your guess is bigger than the secret number. Please try again")
+		} else if guess < secretNumber {
+			fmt.Println("Your guess is less than the secret number. Please try again")
+		} else {
+			fmt.Println("Your guess is corret")
+			break
+		}
+	}
+}
